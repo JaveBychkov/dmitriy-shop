3cf02bb8af532f0fe90f800028c0b35eecec44cc
Dmitriy Bychkov <isumenam@gmail.com>
2018-02-16 16:58:41 +0300

    Check for email uniqueness, new message for postcode validator, new styles

--- profiles/forms.py
+++ profiles/forms.py
@@ -10,6 +10,18 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('first_name', 'last_name', 'email')
 
+    def clean_email(self):
+        # TODO: Find a better way to make email unique.
+        # unique_together ?
+        email = self.cleaned_data['email']
+        initial_email = self.initial.get('email')
+        if initial_email == email:
+            return email
+        if email and User.objects.filter(email=email).exists():
+            raise forms.ValidationError(
+                _('User with given email already exists'), code='invalid')
+        return email
+
 
 class AddressForm(forms.ModelForm):
 
@@ -29,6 +41,13 @@ class UserWEmailCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if email and User.objects.filter(email=email).exists():
+            raise forms.ValidationError(
+                _('User with given email already exists'), code='invalid')
+        return email
 
     def save(self, commit=True):
         user = super().save(commit=False)
